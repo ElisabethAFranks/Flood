a122f0fc70cb16aad6f79bc02822747b32761218
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-01 14:22:00 +0000

    Task 1B Tab Created

---
+++ Task1B.py
@@ -0,0 +1,8 @@
+from floodsystem.geo import stations_by_distance
+
+
+def run():
+    """Requirements for Task 1A"""
+
+    # Returns list of stations and their distance from p
+    stations = stations_by_distance(stations, p)
