c169e7d50587aaf26e9c59d6e4e04cc592becb43
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-01 20:21:35 +0000

    Getting there, just finishing 1D

--- Task 1D.py
+++ Task 1D.py
@@ -1,3 +1,20 @@
-from floodsystem.geo import rivers_with_station
+"""from floodsystem.geo import rivers_with_station
 rivers_with_station.sort()
-print(rivers_with_station[:10])
+print(rivers_with_station[:10])"""
+
+from floodsystem.stationdata import build_station_list
+import floodsystem.geo as geo
+
+
+def run():
+    """Requirements for Task 1D"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    rivers = geo.rivers_with_station(stations)
+    print("There are " + len(rivers) + " rivers." )
+    print(rivers[:10])
+
+    riversandstations = geo.stations_by_river(stations)
+    print(riversandstations["River Thames"])
