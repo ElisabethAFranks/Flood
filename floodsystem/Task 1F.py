557c28923d11eac0163fd196682bb43aa1087135
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-01 20:52:47 +0000

    Something is wrong here, in station check method vs submodule and do demonstration

---
+++ floodsystem/Task 1F.py
@@ -0,0 +1,8 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.station import inconsistent_typical_range_stations
+
+def run():
+    """Requirements for Task 1F"""
+
+    # Build list of stations
+    stations = build_station_list()
