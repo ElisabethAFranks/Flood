b13b75c25b3a92faaeb46b10fe5d14629e146a37
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-01 14:04:11 +0000

    First Attempt Task 1B

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,11 @@
 geographical data.
 
 """
-
-from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+p = (0.,0.)
+stationanddistance = []
+def stations_by_distance(stations, p):
+    for station in stations: 
+        distance = float(haversine(p,station.coordinate, unit=Unit.km))
+        stationanddistance.append(station.name,distance)
+    return stationanddistance
