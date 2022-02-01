a8739a91fcefb2bc851aed40c2549556f591b2dc
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-01 16:16:00 +0000

    Task 1B getting better

--- Task1B.py
+++ Task1B.py
@@ -2,7 +2,13 @@ from floodsystem.geo import stations_by_distance
 
 
 def run():
-    """Requirements for Task 1A"""
+    """Requirements for Task 1B"""
 
     # Returns list of stations and their distance from p
     stations = stations_by_distance(stations, p)
+
+     # Returns list of closest ten stations
+    stations = stations_by_distance(stations[:10])
+
+     # Returns list of furthest ten stations
+    stations = stations_by_distance(stations[-10:])
