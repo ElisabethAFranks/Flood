681e8d1bea6e418e5a78adcb03fe2b0d1c06c826
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-05 12:01:10 +0000

    something to do with testing

---
+++ test_geo.py
@@ -0,0 +1,19 @@
+import floodsystem.geo as geo
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.station import MonitoringStation
+stations = build_station_list()
+Test_station = [MonitoringStation(
+    station_id = 1,
+    measure_id = 10,
+    label = 'test_station',
+    coord = (float(0.1), float(0.1)),
+    typical_range = (1.2,1.5),
+    river = 'River_test',
+    town = 'test_town'), MonitoringStation(
+    station_id = 2,
+    measure_id = 20,
+    label = 'test_station_2',
+    coord = (float(50), float(0)),
+    typical_range = (15,2),
+    river = 'River_test',
+    town = 'test_town')]
