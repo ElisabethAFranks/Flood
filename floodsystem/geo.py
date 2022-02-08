1a6e6bfb6470c34f8755467224c25a14f2dbce6a
Orla Hill <oh292@cam.ac.uk>
2022-02-08 00:33:24 +0000

    trial 3

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -41,3 +41,53 @@ def stations_by_river(stations):
         stationandriver[station] = sorted(stationandriver[station])
         riverandstation = rivers_with_station(stations)
     return riverandstation
+
+
+def stations_within_radius(stations, centre, r):
+    """stations_within_radius(stations, centre, r) returns an alphabetical list of the stations within radius r of the coordinate, centre"""
+    # Calls stations from previous function
+    stations_distance = stations_by_distance(stations, centre)
+
+    #create list of stations in radius
+    stations_in_r = []
+
+    #adds stations to stations_in_r
+    for i, stations in enumerate(stations):
+        if stations_distance[i][2] < r:
+            stations_in_r.append(stations_distance[i][0])
+        else:
+            break
+    
+    #sort stations_in_r alphabetically
+    stations_in_r.sort()
+    
+    return stations_in_r
+
+def rivers_by_station_number(stations, N):
+    """rivers_by_station_number returns a list of tuples(river name, number of stations) sorted by number of stations for the first N rivers"""
+
+    #use dictionary from stations_by_river(stations)
+    stations_by_riv = stations_by_river(stations) 
+
+    #create empty list of rivers
+    rivers_stations = []
+
+    #add number of stations to list
+    for key, value in stations_by_riv.items():
+        rivers_stations.append((key, len(value)))
+
+    #sort list by number of stations
+    rivers_stations = sorted(rivers_stations, key = lambda x:-x[1])  
+
+    output = rivers_stations[:N]
+
+    #sort what happens if nth entry has equal sumber of stations
+    list_complete = False
+    while list_complete == False:
+        if rivers_stations[-N][1] == rivers_stations[-(N+1)]:
+            output.append(rivers_stations[-(N+1)])
+            
+        else:
+            list_complete = True
+
+    return output
