82052f3d39e98182340d4a9a8af6daca6c1326ca
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-08 12:38:17 +0000

    Another day another attempt

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -11,33 +11,33 @@ from floodsystem.station import MonitoringStation
 
 def stations_by_distance(stations, p):
     """function which returns a tuple of the station's name and the station's distance from point p"""
-    stationanddistance = []
+    station_and_distance = []
     for station in stations: 
         distance = float(haversine(p,station.coord))
-        stationanddistance.append(station.name,distance)
-    stationanddistance = sorted([(station.name, station.town, distance[i]) for i, station in enumerate(stations)], key = lambda x:x[2])
-    return stationanddistance
+        station_and_distance.append(station.name,distance)
+    station_and_distance = sorted([(station.name, station.town, distance[i]) for i, station in enumerate(stations)], key = lambda x:x[2])
+    return station_and_distance
 
 def rivers_with_station(stations):
     """function which returns a list of rivers which have a monitoring station"""
-    riverandstation = []
+    river_and_station = []
     for station in stations:
         if station.river == True and station.river in riverandstation == False:
-            riverandstation.append(station.river)
+            river_and_station.append(station.river)
         else:
             continue
-    riverandstation = sorted(riverandstation)
-    return riverandstation
+    river_and_station = sorted(river_and_station)
+    return river_and_station
 
 def stations_by_river(stations):
     """function which returns a dictionary of rivers and a list of the stations on the river"""
-    stationandriver = {}
+    station_and_river = {}
     for station in stations:
-        if not station.river in stationandriver:
-            stationandriver.update({station.river: list([station.name])})
+        if not station.river in station_and_river:
+            station_and_river.update({station.river: list([station.name])})
         else:
-            stationandriver[station.river] = list(stationandriver[station.river]) + [station.name]
-    for station in stationandriver:
-        stationandriver[station] = sorted(stationandriver[station])
-        riverandstation = rivers_with_station(stations)
-    return riverandstation
+            station_and_river[station.river] = list(station_and_river[station.river]) + [station.name]
+    for station in station_and_river:
+        station_and_river[station] = sorted(station_and_river[station])
+        river_and_station = rivers_with_station(stations)
+    return river_and_station
