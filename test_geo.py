56f3c213bc3dfd769f3d59177a6fbcbad534a90d
Orla Hill <oh292@cam.ac.uk>
2022-02-10 15:56:31 +0000

    unnaming

--- test_geo.py
+++ test_geo.py
@@ -41,7 +41,7 @@ def test_stations_within_radius():
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 500)) > len(geo.stations_within_radius(stations, (52.2053, 0.1218), 2))
 
 def test_rivers_by_station_number():
-    #1E
+
     test_list = geo.rivers_by_station_number(stations, 10)
     assert test_list[0][1] >= test_list[1][1]
     assert test_list[0][0] == 'River Thames'
