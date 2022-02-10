a8acda306b19bfce1a3ef664977b0f43030b7de2
Orla Hill <oh292@cam.ac.uk>
2022-02-10 14:44:11 +0000

    final check

--- test_geo.py
+++ test_geo.py
@@ -39,6 +39,7 @@ def test_stations_within_radius():
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 0)) == 0
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 10)) > 0
     assert len(geo.stations_within_radius(stations, (52.2053, 0.1218), 500)) > len(geo.stations_within_radius(stations, (52.2053, 0.1218), 2))
+
 def test_rivers_by_station_number():
     #1E
     test_list = geo.rivers_by_station_number(stations, 10)
