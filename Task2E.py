82709af7fefbbb405a05eda0c905c454bfa8acf1
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-24 21:27:09 +0000

    hehehe

--- Task2E.py
+++ Task2E.py
@@ -1,26 +1,25 @@
 import datetime
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_highest_rel_level
+import numpy as np
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
-    high_stations = flood.stations_highest_rel_level(stations,5)
+    high_stations = stations_highest_rel_level(stations,5)
     list_of_stations = []
     for station in stations:
         for i in range(len(high_stations)):
             if station.name == high_stations[i][0]:
                 list_of_stations.append(station)
     dt = 10
-    dates, levels = fetch_measure_levels(
-        station_cam.measure_id, dt=datetime.timedelta(days=dt))
-
-    # Print level history
-
     for i in range(len(list_of_stations)):
-        dates[i], levels[i] = fetch_measure_levels(list_of_stations[i].measure_id, dt=datetime.timedelta(days=dt)
-        plot.plot_water_levels(list_of_stations[i],dates[i], levels[i])
+        dates = np.empty(6, dtype = object)
+        levels = [None, None, None, None, None, None, None, None, None, None]
+        dates[i], levels[i] = fetch_measure_levels(list_of_stations[i].measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(list_of_stations[i],dates[i], levels[i])
 
 
 if __name__ == "__main__":
