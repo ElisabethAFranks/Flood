f7bf2f670d167c0150d54578730b54bc89589655
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-24 18:30:19 +0000

    N

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,3 +6,7 @@ def stations_level_over_threshold(stations,tol):
         if station.typical_range_consistent() and station.relative_water_level() != None:
             if station.relative_water_level() > tol:
                 list_of_stations_over_tol.append((station, station.relative_water_level()))
+    else:
+        pass
+    list_of_stations_over_tol = sorted(list_of_stations_over_tol, key = lambda x:-x[1])
+    return list_of_stations_over_tol
