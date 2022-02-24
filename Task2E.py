d7ef77a92671502d379d3fb11ba9a9355a6c18cf
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-24 20:41:56 +0000

    okieee

--- Task2E.py
+++ Task2E.py
@@ -7,9 +7,12 @@ from floodsystem.plot import plot_water_levels
 def run():
     stations = build_station_list()
     update_water_levels(stations)
-    
-
-    # Fetch data over past 2 days
+    high_stations = flood.stations_highest_rel_level(stations,5)
+    list_of_stations = []
+    for station in stations:
+        for i in range(len(high_stations)):
+            if station.name == high_stations[i][0]:
+                list_of_stations.append(station)
     dt = 10
     dates, levels = fetch_measure_levels(
         station_cam.measure_id, dt=datetime.timedelta(days=dt))
