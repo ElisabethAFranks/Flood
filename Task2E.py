f8bef4f1849b9a675cb20e76709551f11d1f7fcc
Orla Hill <oh292@cam.ac.uk>
2022-02-26 22:09:06 +0000

    make graphs good

--- Task2E.py
+++ Task2E.py
@@ -12,9 +12,8 @@ def run():
     print("run starts")
     # Build list of stations
     stations = build_station_list()
-    noStations = 5
     # Station name to find
-    station_names = flood.stations_highest_rel_level(stations, noStations)
+    station_names = flood.stations_highest_rel_level(stations, 5)
     
     station_list = []
     for station in stations:
@@ -26,7 +25,7 @@ def run():
     
     # Fetch data over past 2 days
     dt = 10
-    dates = np.empty(noStations, dtype=object)
+    dates = np.empty(6, dtype=object)
     levels = [None, None ,None ,None ,None,None, None ,None ,None ,None]
     for i in range(len(station_list)):
         dates[i], levels[i] = fetch_measure_levels(
