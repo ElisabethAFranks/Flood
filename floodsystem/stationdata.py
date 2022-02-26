125187b787e9e0659ae5266bfea892cc0bdd5af5
Orla Hill <oh292@cam.ac.uk>
2022-02-26 16:58:53 +0000

    imports all gone funny again trying to fix

--- floodsystem/stationdata.py
+++ floodsystem/stationdata.py
@@ -3,21 +3,18 @@
 # SPDX-License-Identifier: MIT
 """This module provides interface for extracting statiob data from
 JSON objects fetched from the Internet and
-
 """
 
-import datafetcher
-from station import MonitoringStation
+from . import datafetcher
+from .station import MonitoringStation
 
 
 def build_station_list(use_cache=True):
     """Build and return a list of all river level monitoring stations
     based on data fetched from the Environment agency. Each station is
     represented as a MonitoringStation object.
-
     The available data for some station is incomplete or not
     available.
-
     """
 
     # Fetch station data
