95ace1b7104e20bedaf878bfb54df42e509f52eb
Elisabeth Franks <eaf48@cam.ac.uk>
2022-02-26 21:28:41 +0000

    please work

--- Task2E.py
+++ Task2E.py
@@ -7,20 +7,22 @@ import numpy as np
 
 def run():
     stations = build_station_list()
-    update_water_levels(stations)
-    high_stations = stations_highest_rel_level(stations,5)
-    list_of_stations = []
+    station_names = stations_highest_rel_level(stations, 5)
+    station_list = []
     for station in stations:
-        for i in range(len(high_stations)):
-            if station.name == high_stations[i][0]:
-                list_of_stations.append(station)
+        for i in range(len(station_names)):
+            if station.name == station_names[i][0]:
+                station_list.append(station)
     dt = 10
-    for i in range(len(list_of_stations)):
-        dates = np.empty(6, dtype = object)
-        levels = [None, None, None, None, None, None, None, None, None, None]
-        dates[i], levels[i] = fetch_measure_levels(list_of_stations[i].measure_id, dt=datetime.timedelta(days=dt))
-        plot_water_levels(list_of_stations[i],dates[i], levels[i])
-
+    dates = np.empty(6, dtype=object)
+    levels = [None, None ,None ,None ,None,None, None ,None ,None ,None]
+    for i in range(len(station_list)):
+        dates[i], levels[i] = fetch_measure_levels(station_list[i].measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station_list[i], dates[i], levels[i])
+    plt.title("station.name")
+    plt.legend(loc="upper left")
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
